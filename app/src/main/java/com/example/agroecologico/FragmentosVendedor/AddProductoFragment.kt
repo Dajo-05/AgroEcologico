bbe485cbe1ff7495f8b45a6e34d0ef217f2be3a6
Dajo-05 <daviscell@hotmail.com>
2021-09-24 14:59:22 -0500

    se agrego el buscar la imagen en el dispositvo

--- app/src/main/java/com/example/agroecologico/FragmentosVendedor/AddProductoFragment.kt
+++ app/src/main/java/com/example/agroecologico/FragmentosVendedor/AddProductoFragment.kt
@@ -1,6 +1,10 @@
 package com.example.agroecologico.FragmentosVendedor
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,13 +17,19 @@ import androidx.core.view.get
 import com.example.agroecologico.R
 import com.example.agroecologico.data.UnidadData
 import com.example.agroecologico.databinding.FragmentAddProductoBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.*
 
 
 class AddProductoFragment : Fragment() {
 
+    // identificar del inten
+    private val RC_GALERY = 7
+    private val PATH_IMAGENES = "Imagenes"
+
     private lateinit var mBinding: FragmentAddProductoBinding
     private lateinit var database: DatabaseReference
+    private var mImageSeleccionarUri: Uri?=null
 
 
     override fun onCreateView(
@@ -38,10 +48,39 @@ class AddProductoFragment : Fragment() {
 
     }
 
-   /* override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mBinding.btnSeleccinar.setOnClickListener { abrirGaleria() }
 
-    }*/
+    }
+
+    private fun abrirGaleria() {
+        // creamos el intent para abrir la galeria
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        Log.d("ingresoro","abrior a la galeria")
+        startActivityForResult(intent, RC_GALERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("fuerad el if 1","$resultCode")
+        if (resultCode == Activity.RESULT_OK){
+            Log.d("dentro del if1 pt1","$resultCode")
+            Log.d("dentro del if1 pt2","$RC_GALERY")
+            if (requestCode == RC_GALERY){
+                mImageSeleccionarUri = data?.data
+                // var datosImag = mImageSeleccionarUri
+                Log.d("esto es antes del erro", mImageSeleccionarUri.toString())
+                mBinding.ivPhoto.setImageURI(mImageSeleccionarUri)
+            }else{
+                Snackbar.make(mBinding.root, "Mensaje2", Snackbar.LENGTH_SHORT).show()
+            }
+        }else{
+            Snackbar.make(mBinding.root, "Mensjae 1", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
 
     private fun Datosslect() {
         val query = FirebaseDatabase.getInstance().reference.child("UnidadVenta")
@@ -66,7 +105,7 @@ class AddProductoFragment : Fragment() {
           }
 
           override fun onCancelled(error: DatabaseError) {
-              TODO("Not yet implemented")
+              Log.d("Elemto select", error.toString())
           }
 
       }
